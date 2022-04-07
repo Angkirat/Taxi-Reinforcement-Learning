1c9974dc3e0907dfaadc8af683e6dd10644a6456
Angkirat Sandhu <angkirat@gmail.com>
2022-04-07 23:45:01 +0100

    fixed runtime issues and added logs

--- dqn_learning.py
+++ dqn_learning.py
@@ -1,34 +1,64 @@
 import gym
+import pickle
 import logging
 import numpy as np
 from tqdm import tqdm
 import tensorflow as tf
+from statistics import mean
+from datetime import datetime
 
 import util
-from q_table_learning import QTable_Learning
 from gym_environment import GymEnvironment
 
+logdir = "logs/DQN/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+file_writer = tf.summary.create_file_writer(logdir + "/metrics")
+file_writer.set_as_default()
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
-class DQN_Learning(QTable_Learning):
+
+class DQN_Learning:
     def __init__(
         self,
         env: GymEnvironment, alpha: float = 1,
         epsilon: float = 0.05, gamma: float = 0.8,
-        model_file: str = None, learning_rate: float = 1e-5,
-        initial_state_collection: int = 1e5
+        learning_rate: float = 1e-5,
+        hidden_layer_units: list = [100, 50], dropout: list = [0.2, 0.2],
+        initial_state_collection: int = 1e5, model_save_path: str = 'ModelOutput/DQN_leanring'
     ) -> None:
-        super().__init__(env, alpha, epsilon, gamma, None)
-        self.model_file = model_file
+        self.env = env
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
         self.dqn_learning_rate = learning_rate
-        self.model = self.create_model()
+        self.model = None
         self.buffer = util.ReplayBuffer(1e10, 1)
         self.evaluation_env = env
         self.initial_state_collection = initial_state_collection
+        self.hidden_layer_units = hidden_layer_units
+        self.dropout = dropout
+        self.model_save_path = model_save_path
 
-    def create_model(self, hidden_layer_units: list = [100, 50], dropout: list = [0.5, 0.5]):
+    def training_main(self, training_episode_count: int, test_episode_count: int, evaluation_percentage: int):
+        self.model = self.create_model()
+        training_loss, evaluation_metrics = self.train_model_episode(
+            training_episode_count,
+            test_episode_count,
+            evaluation_percentage
+        )
+        evaluation_ep_reward, evaluation_ep_step = self.evaluate_model(200)
+        summary_model = {
+            'training_loss': training_loss,
+            'evaluation_metrics': evaluation_metrics,
+            'evaluation_ep_reward': evaluation_ep_reward,
+            'evaluation_ep_step': evaluation_ep_step
+        }
+        pickle.dump(summary_model, open('ModelOutput/DQN-Metrics.pkl', 'wb'))
+
+
+    def create_model(self):
         model = tf.keras.Sequential(name="DQNModel")
         model.add(tf.keras.Input(shape=self.env.obs_shape))
-        for i, (units, drop) in enumerate(zip(hidden_layer_units, dropout)):
+        for i, (units, drop) in enumerate(zip(self.hidden_layer_units, self.dropout)):
             model.add(tf.keras.layers.Dense(
                 units,
                 activation=tf.keras.activations.relu,
@@ -60,7 +90,7 @@ class DQN_Learning(QTable_Learning):
         return model
 
     def compute_action(self, observation: np.ndarray):
-        predicted_qvalue = self.model.predict(np.array(observation))
+        predicted_qvalue = self.model.predict(np.array([observation]))
         return predicted_qvalue[0].argmax()
 
     def random_action(self, observation: np.ndarray):
@@ -69,7 +99,7 @@ class DQN_Learning(QTable_Learning):
         return self.compute_action(observation)
 
     def q_value_calculation(self, state: np.ndarray, next_state: np.ndarray, reward: int, action: int):
-        state_qvalue = max(self.model.predict(np.array([state, next_state])))
+        state_qvalue = self.model.predict(np.array([state, next_state]))
         sample = self.alpha * (reward + (self.gamma * max(state_qvalue[1])))
         state_q_value = state_qvalue[0]
         state_action_qvalue = (1 - self.alpha) * state_q_value[action]
@@ -96,7 +126,8 @@ class DQN_Learning(QTable_Learning):
             batch_size=batch_size)
         q_value_batch = np.array([
             self.q_value_calculation(state, next_state, reward, action)
-            for state, next_state, reward, action in zip(state_batch, action_batch, reward_batch, new_state_batch)
+            for state, action, reward, next_state in
+            zip(state_batch, action_batch, reward_batch, new_state_batch)
         ])
         return state_batch, q_value_batch
 
@@ -120,31 +151,54 @@ class DQN_Learning(QTable_Learning):
             observation_batch, q_value_batch = self.batch_creation(
                 batch_size=batch)
 
-    def train_model_episode(self, episode_count: int):
+    def load_episode_buffer(self, episode_buffer: util.ReplayBuffer):
+        observation = self.env.env.reset()
+        while True:
+            idx = episode_buffer.store_frame(observation)
+            action = self.random_action(observation)
+            observation, reward, done, _ = self.env.env.step(action)
+            episode_buffer.store_effect(idx, action, reward, done)
+            if done:
+                break
+
+    def train_model_episode(self, training_episode_count: int, test_episode_count: int, evaluation_percentage: int):
+        eval_step = (evaluation_percentage/100) * training_episode_count
         print('\n\n')
         print('Training DQN model (episode)')
-        for _ in tqdm(range(episode_count)):
+        training_loss = []
+        evaluation_metrics = []
+        for i in range(training_episode_count):
             episode_buffer = util.ReplayBuffer(1e5, 1)
-            observation = self.env.env.reset()
-
-            # play full episode and store in buffer
-            while True:
-                idx = episode_buffer.store_frame(observation)
-                action = self.random_action(observation)
-                observation, reward, done, _ = self.env.env.step(action)
-                episode_buffer.store_effect(idx, action, reward, done)
-                if done:
-                    break
-
-            state_batch, action_batch, reward_batch, new_state_batch = episode_buffer.sample_all()
+            self.load_episode_buffer(episode_buffer)
+            state_batch, action_batch, reward_batch, new_state_batch, _ = episode_buffer.sample_all()
 
             # calculate Q Value of the model
             q_value_batch = np.array([
                 self.q_value_calculation(state, next_state, reward, action)
-                for state, next_state, reward, action in zip(state_batch, action_batch, reward_batch, new_state_batch)
+                for state, action, reward, next_state in
+                zip(state_batch, action_batch, reward_batch, new_state_batch)
             ])
 
-            self.model.train_on_batch(state_batch, q_value_batch)
+            mean_training_loss, mse_training_loss = self.model.train_on_batch(
+                state_batch, q_value_batch)
+            training_loss.append([mean_training_loss, mse_training_loss])
+
+            print(f'DQN Training; Step: {i}; MSE Training Loss: {mse_training_loss}; Mean Training Loss: {mean_training_loss}')
+
+            if i % eval_step:
+                logging.info(
+                    f'DQN Training; Step: {i}; MSE Training Loss: {mse_training_loss}; Mean Training Loss: {mean_training_loss}')
+                evaluation_ep_reward, evaluation_ep_step = self.evaluate_model(
+                    test_episode_count)
+                logging.info(
+                    f'Train Evaluation; Average Reward: {mean(evaluation_ep_reward)}; Average Step: {mean(evaluation_ep_step)}')
+                evaluation_metrics.append([evaluation_ep_reward, evaluation_ep_step])
+                self.save_model()
+                self.epsilon = self.epsilon * 0.8
+            else:
+                logging.debug(
+                    f'DQN Training; Step: {i}; MSE Training Loss: {mse_training_loss}; Mean Training Loss: {mean_training_loss}')
+        return training_loss, evaluation_metrics
 
     def evaluate_model(self, episode_count: int):
         evaluation_ep_reward = []
@@ -168,3 +222,9 @@ class DQN_Learning(QTable_Learning):
             logging.info(
                 f'Testing Iteration {i}; total Steps Taken {episode_step_count}; Rewards gained: {episode_reward}')
         return evaluation_ep_reward, evaluation_ep_step
+
+    def save_model(self):
+        self.model.save(self.model_save_path)
+
+    def load_model(self):
+        self.model.load_model(self.model_save_path)
